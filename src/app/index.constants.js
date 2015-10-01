bba4b79415bc37019e2a4a53687fc64e91cb04ea
jmanock <jonmanock@gmail.com>
2015-10-01 08:18:16 -0400

    getting gulp to work

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('codeNameMean')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('codeNameMean', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router']);
+
+})();
