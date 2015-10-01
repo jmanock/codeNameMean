7858a09dba609871d1b328b4a08bcfaca99c12fb
jmanock <jonmanock@gmail.com>
2015-10-01 19:41:24 -0400

    testing a new app

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,32 +8,6 @@
   /** @ngInject */
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1443701368359;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    $http.get('http://www.hockey-reference.com/leagues/NHL_2016_games.html')
     }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
 })();
