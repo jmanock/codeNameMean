09b5550137bec0f0922726fa265d33e039b60d43
jmanock <jonmanock@gmail.com>
2015-10-02 08:33:43 -0400

    has to be a better way to do this

--- src/app/main/nhl.js
+++ src/app/main/nhl.js
@@ -1,23 +1,18 @@
-'use strict';
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
+(function(){
+  'use strict';
+  var request = require('request');
+  var cheerio = require('cheerio');
+  var url = 'http://www.hockey-reference.com/leagues/NHL_2016_games.html';
 
-var url = 'http://www.hockey-reference.com/leagues/NHL_2016_games.html';
-var something = [];
-request(url, function(error, response, body){
-
-  if(!error && response.statusCode === 200){
-    var $ = cheerio.load(body);
-    // var visitTeam = $('.left > a').text();
-    // var homeTeam = $('.right > a').text();
-    var date = $('tr > td');
-    
-    /* Get
-      ~ Home Team
-      ~ Visiting Team
-      ~ Date
-    */
-  }
-
-});
+  request(url, function(error, response, body){
+    if(!error && response.statusCode === 200){
+      var $ = cheerio.load(body);
+      var rows;
+      for(var i = 0; i<1230; i++){
+        rows = '.data-row='+i;
+        var knew = $(rows);
+        console.log(knew);
+      }
+    }
+  });
+})();
