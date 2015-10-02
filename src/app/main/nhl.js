6bf5c9742dbc6fad9e7e466aa945ac43e06360dc
jmanock <jonmanock@gmail.com>
2015-10-02 13:25:15 -0400

    working on a refactor

--- src/app/main/nhl.js
+++ src/app/main/nhl.js
@@ -1,8 +1,10 @@
 (function(){
   'use strict';
+
   var request = require('request');
   var cheerio = require('cheerio');
   var moment = require('moment');
+
   var url = 'http://www.hockey-reference.com/leagues/NHL_2016_games.html';
   var something = [];
   var vTeam = [];
@@ -10,49 +12,26 @@
   var dates = [];
   var cut = [];
   var final = [];
+
   request(url, function(error, response, body){
     if(!error && response.statusCode === 200){
       var $ = cheerio.load(body);
-      var link = $('td > a');
-      var date = $('tr').next().text();
+      var teams = $('td > a');
+      // var date = $('tr').next().text();
 
-
-
-      link.each(function(i,links){
-        var knames = links.children;
-        var date = links.parent.attribs.csk;
+      teams.each(function(i, team){
+        var names = team.children;
+        var date = team.parent.attribs.csk;
         dates.push(date);
 
-        for(var j = 0; j<knames.length; j++){
-          var games = knames[j].data;
-          something.push(games);
+        for(var a = 0; a<names.length; a++){
+          var games = names[a].data;
+           something.push(games);
         }
       });
     }
-    for(var x = 0; x<something.length; x++){
-      if(x % 2 === 0){
-        vTeam.push(something[x]);
-      }else{
-        hTeam.push(something[x]);
-      }
-    }
 
-    for(var k = 0; k<dates.length; k++){
-      if(k % 2 === 0){
-        var kick = dates[k].slice(4,12);
-        var a = moment(kick, 'YYYYMMDD').format('MMM Do YYYY');
-        cut.push(a);
-      }
+    
 
-    }
-
-    for(var b = 0; b<cut.length && b<hTeam.length && b<vTeam.length; b++){
-      final.push({
-        Date:cut[b],
-        Home:hTeam[b],
-        Vistor:vTeam[b]
-      });
-    }
-    console.log(final.length);
   });
 })();
