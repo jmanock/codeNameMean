f98fdf3ef196fbf6a8fb28596e0d120b95d73b2e
jmanock <jonmanock@gmail.com>
2015-10-02 04:40:37 -0400

    working on getting the schedule script set up

--- src/app/main/nhl.js
+++ src/app/main/nhl.js
@@ -1,2 +1,16 @@
-var http = require('http');
- 
+'use strict';
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var url = 'http://www.hockey-reference.com/leagues/NHL_2016_games.html';
+var something = [];
+request(url, function(error, response, body){
+
+  if(!error && response.statusCode === 200){
+    var $ = cheerio.load(body);
+    var links = $('td > a').text();
+    something.push(links);
+    console.log(something.length);
+  }
+});
