616eb9868630639ae9adbf81c48fb60dba501243
jmanock <jonmanock@gmail.com>
2015-10-07 13:15:55 -0400

    not working the way i thought it would more rethinking

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,7 +8,7 @@
   /** @ngInject */
   function MainController($firebaseArray) {
     var vm = this;
-    var url = 'https://fireseedangular.firebaseio.com/Weeks/Week1';
+    var url = 'https://fireseedangular.firebaseio.com/Weeks/Week1/0/Games';
     var ref = new Firebase(url);
     vm.weeks = $firebaseArray(ref);
 
