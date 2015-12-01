1d3ace01a6b8c900fdd9c1295fb3381daaf437d0
jmanock <jonmanock@gmail.com>
2015-12-01 07:05:41 -0500

    ok got the stupid thing to show up

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,18 +6,30 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($scope) {
-    
-    $.getJSON('http://cors.io/?u=http://www.pgatour.com/data/r/current/leaderboard-v2.json',function(data){
+  function MainController($scope, $http) {
+    var something = [];
+    // $.getJSON('http://cors.io/?u=http://www.pgatour.com/data/r/current/leaderboard-v2.json',function(data){
+    //   var player = data.leaderboard.players;
+    //   player.forEach(function(x){
+    //     var firstName = x.player_bio.first_name;
+    //     var lastName = x.player_bio.last_name;
+    //     var fullName = firstName + ' '+ lastName;
+    //     something.push(fullName);
+    //   });
+    //   $scope.players = something;
+    //   console.log(something);
+    // });
+    $http.get('http://cors.io/?u=http://www.pgatour.com/data/r/current/leaderboard-v2.json')
+    .success(function(data){
+      var players = [];
       var player = data.leaderboard.players;
-      player.forEach(function(x){
+      angular.forEach(player, function(x){
         var firstName = x.player_bio.first_name;
         var lastName = x.player_bio.last_name;
         var fullName = firstName + ' '+ lastName;
-        // console.log(fullName);
-
+        players.push(fullName);
       });
-
+      $scope.players = players;
     });
 
   }
