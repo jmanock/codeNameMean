b1c5fe32f33556a4c75b4f27dd76adc1681f73b8
jmanock <jonmanock@gmail.com>
2016-01-02 13:34:51 -0500

    added dumb points

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -12,42 +12,42 @@
   // }
 
   function StandingsController($scope){
-    var something = {
-      "modules":
-        [
-          {
-            "title":"name of module1",
-            "description":"description of module1",
-            "weeks":[{"id":1, "title":"Week 01"}]
-          },
-          {
-            "title":"name of module2",
-            "description":"description of module2",
-            "weeks":[{"id":2, "title":"Week 02"},{"id":3, "title":"Week 03"}]
-          },
-          {
-            "title":"name of module3",
-            "description":"description of module3",
-            "weeks":[{"id":4, "title":"Week 04"},{"id":5, "title":"Week fucking 5"}]
-          }
-        ]
-    };
-    $scope.ocw = something;
+    // var something = {
+    //   "modules":
+    //     [
+    //       {
+    //         "title":"name of module1",
+    //         "description":"description of module1",
+    //         "weeks":[{"id":1, "title":"Week 01"}]
+    //       },
+    //       {
+    //         "title":"name of module2",
+    //         "description":"description of module2",
+    //         "weeks":[{"id":2, "title":"Week 02"},{"id":3, "title":"Week 03"}]
+    //       },
+    //       {
+    //         "title":"name of module3",
+    //         "description":"description of module3",
+    //         "weeks":[{"id":4, "title":"Week 04"},{"id":5, "title":"Week fucking 5"}]
+    //       }
+    //     ]
+    // };
+    // $scope.ocw = something;
 
     var works = {
       "Users":
       [
         {
           "User":"Jim Chaser",
-          "team":[{"Player":"Tiger Woods"},{"Player":"Jordan Speith"},{"Player":"Dustin Johnson"}, {"Player":"Sergio Garcia"}]
+          "team":[{"Player":"Tiger Woods", "Points":50},{"Player":"Jordan Speith", "Points":200},{"Player":"Dustin Johnson", "Points":300}, {"Player":"Sergio Garcia", "Points":123}]
         },
         {
           "User":"Brit Fernandez",
-          "team":[{"Player":"Jordan Speith"}, {"Player":"Jason Day"}, {"Player":"Rory Mac"},{"Player":"Sergio Garcia"}]
+          "team":[{"Player":"Jordan Speith", "Points":132}, {"Player":"Jason Day", "Points":321}, {"Player":"Rory Mac", "Points":234},{"Player":"Sergio Garcia", "Points":243}]
         },
         {
           "User":"Jon Mac",
-          "team":[{"Player":"Zach Jonshon"}, {"Player":"Brooks Kopa"},{"Player":"Scott Stallings"}, {'Player':"Tony Funu"}]
+          "team":[{"Player":"Zach Jonshon", "Points":432}, {"Player":"Brooks Kopa", "Points":555},{"Player":"Scott Stallings", "Points":345}, {'Player':"Tony Funu", "Points":999}]
         }
       ]
     };
